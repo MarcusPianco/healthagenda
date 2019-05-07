a7efe9070614675d5e64d1cfa36c6addf0b61ab0
Marcus  Pianco <pianco@MacBook-Pro-de-Marcus.local>
2019-05-07 15:31:27 -0300

    Define project structure and configure ant-design dependency

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu } from 'antd';
+import 'antd/dist/antd.css';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Content, Footer } = Layout;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '68px' }}
+          >
+            <Menu.Item key="1">Login</Menu.Item>
+            <Menu.Item key="2">Registrar</Menu.Item>
+          </Menu>
+        </Header>
+        <Content />
+        <Footer />
+      </Layout>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Agenda/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Agenda extends React.Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <h1>Olá</h1>
+      </div>
+    );
+  }
+}
+
+export default Agenda;
